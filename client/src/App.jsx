210fc7168a20b3675fcaad815ef2d439273a8e22
harikrishnaaa321 <bharikrishna64@gmail.com>
2024-07-13 03:36:51 +0530

    sign up page completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,8 +14,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
-        <Route path="/sign-in" element={<SignIn/>} />
-        <Route path="/sign-up" element={<SignUp/>} />
+        <Route path="/signin" element={<SignIn/>} />
+        <Route path="/signup" element={<SignUp/>} />
         <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="/projects" element={<Projects/>} />
       </Routes>
